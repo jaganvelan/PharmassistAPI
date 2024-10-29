a978b4fdfb3777beda4b51454f656c1322da4e10
jaganvelan <jaganv14102002@gmail.com>
2024-10-29 14:36:05 +0530

    Pharmassist project initialized

---
+++ src/main/java/com/example/pharmassist/PharmassistApi/PharmassistApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmassist.PharmassistApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmassistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmassistApiApplication.class, args);
+	}
+
+}
